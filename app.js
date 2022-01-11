b2df39a7947ae4fcf3c19a7fa86537f430d04c0d
zircadia <76924125+zircadia@users.noreply.github.com>
2022-01-11 15:51:47 -0600

    commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
 
-server.listen(3000);
+app.use('/', (req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
