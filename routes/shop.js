3da011c52546578dc8f3e856a33d25bac7389b8b
zircadia <76924125+zircadia@users.noreply.github.com>
2022-01-14 16:30:30 -0600

    commit refinements

--- routes/shop.js
+++ routes/shop.js
@@ -9,8 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop', path: '/', hasProducts: products.length > 0,
-    activeShop: true, productCSS: true,
+  res.render('shop', {prods: products, docTitle: 'Shop', path: '/'
     });
 });
 
